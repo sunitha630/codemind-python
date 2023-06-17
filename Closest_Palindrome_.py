cce03f88a103db5d60204e4bb7773bf59a4bdc68
suneeta somisetti <121595183+sunitha630@users.noreply.github.com>
2023-06-17 22:11:19 +0530

    def pal(num):     if num==int(str(num)[::-1]):         return True     else:         return False n=int(input()) high=n+1 while not pal(high):     high=high+1 low=n-1 while not pal(low):     low=low-1 if high-n==n-low:     print(low,high) elif high-n>n-low:     print(low) else:     print(high)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,18 +1,18 @@
-def is_pal(num):
+def pal(num):
     if num==int(str(num)[::-1]):
         return True
     else:
         return False
-num=int(input())
-low=num-1
-while not is_pal(low):
-    low=low-1
-high=num+1
-while not is_pal(high):
+n=int(input())
+high=n+1
+while not pal(high):
     high=high+1
-if high-num==num-low:
+low=n-1
+while not pal(low):
+    low=low-1
+if high-n==n-low:
     print(low,high)
-elif high-num>num-low:
+elif high-n>n-low:
     print(low)
 else:
     print(high)
